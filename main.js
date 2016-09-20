16dcc3f03aa9cbb5e3a849a05cea5cd996abc76b
Pratik Varshney <pratikvarshney@live.com>
2016-09-20 14:48:18 -0700

    Modified js

---
+++ main.js
@@ -0,0 +1,82 @@
+window.onload = function() {
+	document.body.onscroll = updateHeader;
+	setMenu();
+	hideMenu();
+}
+
+function setMenu() {
+	var headerList = document.getElementsByClassName("content")[0].getElementsByClassName("title");
+	var menu = document.getElementById("menu");
+	var menuData = document.getElementById("menu-data");
+	document.getElementById("menu-title").onclick = hideMenu;
+
+	for (var i = 0; i < headerList.length; i++) {
+		var elem = document.createElement("div");
+		elem.setAttribute("data-id", "#" + headerList[i].parentNode.id);
+		elem.onclick = function() {
+			hideMenu();
+			window.location.href = this.getAttribute("data-id");
+		};
+		elem.innerHTML = headerList[i].outerHTML;
+		menuData.appendChild(elem);
+		headerList[i].onclick = showMenu;
+	}
+	menu.style.height = "0";
+	menu.style.overflow = "hidden";
+}
+
+function showMenu() {
+	var menu = document.getElementById("menu");
+	var menuData = document.getElementById("menu-data");
+	menu.style.height = (menuData.clientHeight) + "px";
+	menu.style.overflow = "visible";
+	menu.style.opacity = "1";
+	var content = document.getElementById("content");
+	var contentBody = document.getElementById("content-body");
+	content.style.height = "0";
+	content.style.overflow = "hidden";
+	content.style.opacity = "0";
+	document.getElementsByTagName("header")[0].style.display = "none";
+}
+
+function hideMenu() {
+	var menu = document.getElementById("menu");
+	var menuData = document.getElementById("menu-data");
+	menu.style.height = "0";
+	menu.style.overflow = "hidden";
+	menu.style.opacity = "0";
+	var content = document.getElementById("content");
+	var contentBody = document.getElementById("content-body");
+	content.style.height = (contentBody.clientHeight) + "px";;
+	content.style.overflow = "visible";
+	content.style.opacity = "1";
+	document.getElementsByTagName("header")[0].style.display = "";
+}
+
+function updateHeader() {
+	var doc = document.documentElement;
+	var top = (window.pageYOffset || doc.scrollTop) - (doc.clientTop || 0);
+	var currentIndex = -1;
+	var headerList = document.getElementsByClassName("content")[0].getElementsByClassName("title");
+	for (var i = 0; i < headerList.length; i++) {
+		if (headerList[i].getBoundingClientRect().top < 0) {
+			currentIndex = i;
+		} else {
+			break;
+		}
+	}
+	var h = document.getElementsByTagName("header")[0];
+	var margin = 0;
+	var html = "";
+	if (currentIndex != -1) {
+		html = headerList[currentIndex].outerHTML;
+		if (currentIndex + 1 < headerList.length) {
+			var offset = headerList[currentIndex + 1].getBoundingClientRect().top - headerList[currentIndex].clientHeight;
+			if (offset < 0) {
+				margin = offset;
+			}
+		}
+	}
+	h.innerHTML = html;
+	h.style.marginTop = margin;
+}
