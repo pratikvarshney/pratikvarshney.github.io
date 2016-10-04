4ed205b770f5d1bb4e63e49732addd719d681fba
Pratik Varshney <pratikvarshney@live.com>
2016-10-04 02:13:36 -0700

    v2 : added courses

--- new/assets/js/main.js
+++ new/assets/js/main.js
@@ -72,6 +72,19 @@ function setMenu() {
 	}
 }
 
+function toggleCourses(obj){
+	var cNode = obj.parentNode.getElementsByClassName("courses")[0];
+	if(obj.getAttribute("courses-visible") == "true"){
+		cNode.classList.add("hidden");
+		obj.setAttribute("courses-visible", "false");
+		obj.innerHTML = "View courses";
+	}else{
+		cNode.classList.remove("hidden");
+		obj.setAttribute("courses-visible", "true");
+		obj.innerHTML = "Hide courses";
+	}
+}
+
 function showSection(id) {
 	hideMenu();
 	var sid = document.getElementById(id);
